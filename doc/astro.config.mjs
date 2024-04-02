81808f5bb0877f0bf50026c78076c3d4445e68c5
maxime-charles.buisson <maxime-charles.buisson@epita.fr>
2024-04-02 12:20:57 +0200

    fix(deploy): base path + changed redirection path

--- doc/astro.config.mjs
+++ doc/astro.config.mjs
@@ -14,7 +14,7 @@ export default defineConfig({
 	 				label: 'Guides',
 	 				items: [
 	 					// Each item here is one entry in the navigation menu.
-	 					{ label: 'Example Guide', link: '/guides/example/' },
+	 					{ label: 'Example Guide', link: 'documentations/guides/example/' },
 	 				],
 	 			},
 	 			{
@@ -26,5 +26,5 @@ export default defineConfig({
 	 ],
 
 	site: 'https://maxime2025.github.io',
- 	base: '/documentations',
+ 	base: 'documentations',
 });
